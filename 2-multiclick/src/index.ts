354f81e2a03f5185ba8a0f37e8485d0038afba5a
Patrick Latorre <ptrckltrr@gmail.com>
2019-12-09 23:52:16 +0800

    Add implementation of multiclick listener

--- 2-multiclick/src/index.ts
+++ 2-multiclick/src/index.ts
@@ -2,8 +2,17 @@ import * as $ from 'jquery'
 import MulticlickListener from './MulticlickListener'
 import { Subject } from 'rxjs'
 
-// const multiclickListener = new MulticlickListener(document, {
-//   onMulticlick(count: number) {
-//     console.log(`I clicked x times`)
-//   }
-// })
+const multiclickListener = new MulticlickListener(document, {
+  onMulticlick(count: number) {
+    // $('.count').text(`Clicked ${count}x`).show()
+    // setTimeout(() => $('.count').hide(), 1000)
+  },
+
+  onEachMulticlick() {
+    // $('.prompt').text('Multiclicking..').show()
+  },
+
+  onTimeout() {
+    // $('.prompt').hide()
+  }
+})
