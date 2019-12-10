7c999ec5bc5adb8e381a9f52f2eade9786f56aca
Patrick Latorre <ptrckltrr@gmail.com>
2019-12-10 11:50:35 +0800

    Post-session

--- 1-helloworld/src/index.ts
+++ 1-helloworld/src/index.ts
@@ -1,7 +1,34 @@
-import { from } from 'rxjs'
+import { from, Subject } from 'rxjs'
 import { filter, map, tap } from 'rxjs/operators'
 
 const log = console.log
 
 // 'Hello world' as array of characters
-const characters = 'Hello world!'.split('')
+const characters: string[] = 'Hello world!'.split('')
+
+const letter$ = new Subject()
+const _letter$ = from(characters).pipe(
+  tap(letter => letter$.next(letter))
+)
+
+
+
+const ascii$ = letter$.pipe(
+  map((letter: string) => letter.charCodeAt(0))
+)
+
+
+const onLetter = letter$.subscribe({
+  next(letter) {
+    log(letter)
+  }
+})
+
+const onAscii = ascii$.subscribe({
+  next(ascii) {
+    log('^-------' + ascii)
+  }
+})
+
+
+_letter$.subscribe()
